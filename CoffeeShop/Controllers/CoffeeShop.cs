abc2a274bb3a89b372fe6a46e403bd33ec476b39
Mike C <Michch333@gmail.com>
2021-01-25 18:18:51 -0500

    first commit, working registration page

---
+++ CoffeeShop/Controllers/CoffeeShop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoffeeShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class CoffeeShop : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RegisterView()
+        {
+            return View();
+        }
+        public IActionResult RegistrationConformation(RegisterViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
